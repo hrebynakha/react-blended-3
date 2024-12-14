dda129dc2097da89969a1f67822be27efc46098c
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-12-14 16:13:42 +0200

    add t1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
 import Heading from './components/Heading/Heading';
+import { lazy, Suspense } from 'react';
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+const Home = lazy(() => import('./pages/Home'));
+const SearchCountry = lazy(() => import('./pages/SearchCountry'));
+const Country = lazy(() => import('./pages/Country'));
+import Header from './components/Header/Header';
 
 export const App = () => {
-  return <Heading title="App" bottom />;
+  return (
+    <>
+      <Header />
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country" element={<SearchCountry />} />
+          <Route path="/country/:countryId" element={<Country />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from 'react';
 import Container from '../components/Container/Container';
 import Heading from '../components/Heading/Heading';
 import Section from '../components/Section/Section';
+import Country from './Country';
+import CountryList from '../components/CountryList/CountryList';
+import { getCountries } from '../service/countryApi';
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getCountries();
+        setCountries(res);
+      } catch {
+      } finally {
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
+        <CountryList countries={countries} />
         <Heading title="Home" bottom />
       </Container>
     </Section>
